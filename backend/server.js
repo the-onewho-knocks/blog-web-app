a1f2ab8a78771f6c97244e5087128bb619edf824
the-onewho-knocks <borsehardik@gmail.com>
2026-02-15 16:34:11 +0530

    because of the previous commit the production failed therefore this is a roll back commit

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:3100',
-  'http://localhost:5173',
-  'https://lustrous-moxie-7f7c56.netlify.app'
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1 || origin.endsWith('.netlify.app')) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-app.use(cors(corsOptions));
+const cors = require("cors");
 
 const blogRoutes = require("./routes/blogroutes");
 const authRoutes = require("./routes/authroutes");
