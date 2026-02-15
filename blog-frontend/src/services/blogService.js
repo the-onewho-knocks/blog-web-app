f961126ad72c319661d6ab2ac8669c19651a9631
the-onewho-knocks <borsehardik@gmail.com>
2026-02-15 16:31:43 +0530

    fix: Add CORS for Netlify

--- blog-frontend/src/services/blogService.js
+++ blog-frontend/src/services/blogService.js
@@ -1,44 +1,38 @@
-import axios from 'axios';
+import axios from "axios";
 
-// Set base URL based on environment
-const API_BASE_URL = import.meta.env.VITE_API_URL || '/api';
-
-axios.defaults.baseURL = API_BASE_URL;
-
-const API_URL = '/blogs';
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+});
 
 export const blogService = {
-  // Get all blogs
+
   getAllBlogs: async () => {
-    const response = await axios.get(API_URL);
-    return response.data;
+    const res = await api.get("/blogs");
+    return res.data;
   },
 
-  // Get single blog
   getBlog: async (id) => {
-    const response = await axios.get(`${API_URL}/${id}`);
-    return response.data;
+    const res = await api.get(`/blogs/${id}`);
+    return res.data;
   },
 
-  // Create blog
   createBlog: async (formData) => {
-    const response = await axios.post(API_URL, formData, {
+    const res = await api.post("/blogs", formData, {
       headers: {
-        'Content-Type': 'multipart/form-data'
+        "Content-Type": "multipart/form-data"
       }
     });
-    return response.data;
+    return res.data;
   },
 
-  // Update blog
   updateBlog: async (id, data) => {
-    const response = await axios.put(`${API_URL}/${id}`, data);
-    return response.data;
+    const res = await api.put(`/blogs/${id}`, data);
+    return res.data;
   },
 
-  // Delete blog
   deleteBlog: async (id) => {
-    const response = await axios.delete(`${API_URL}/${id}`);
-    return response.data;
+    const res = await api.delete(`/blogs/${id}`);
+    return res.data;
   }
+
 };
